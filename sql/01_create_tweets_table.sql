a0b7ff82b762b14f1506e8119b9a0a5096e8edd4
Sergey Eroshenkov <itechmeat@gmail.com>
2024-12-30 08:32:32 +0100

    fix sql

--- sql/01_create_tweets_table.sql
+++ sql/01_create_tweets_table.sql
@@ -1,20 +1,22 @@
 CREATE TABLE IF NOT EXISTS tweets (
-    id SERIAL PRIMARY KEY,
-    tweet_id BIGINT UNIQUE NOT NULL,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    tweet_id TEXT UNIQUE NOT NULL,
     tweet_user_name TEXT NOT NULL,
     tweet_user_nick TEXT NOT NULL,
     tweet_text TEXT NOT NULL,
     tweet_full_text TEXT NOT NULL,
-    tweet_created_at_datetime TIMESTAMP NOT NULL,
+    tweet_created_at_datetime TIMESTAMP WITH TIME ZONE NOT NULL,
     tweet_retweet_count INTEGER DEFAULT 0,
     tweet_likes INTEGER DEFAULT 0,
     tweet_photo_urls TEXT[] DEFAULT NULL,
     tweet_lang TEXT,
+    tweet_in_reply_to TEXT,
     tweet_view_count INTEGER DEFAULT 0,
+    is_tweet_liked BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     first_seen_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE INDEX IF NOT EXISTS idx_tweets_tweet_id ON tweets(tweet_id);
-CREATE INDEX IF NOT EXISTS idx_tweets_created_at ON tweets(created_at); 
+CREATE INDEX IF NOT EXISTS idx_tweets_created_at ON tweets(created_at);
